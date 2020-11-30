2a83147aeebf01a4685d411d939b603e0ce5f12e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-30 20:03:35 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet (timeinput) {
+  let HH = parseInt(timeinput.split(":")[0],10);
+  if (HH < 12) {
+    return "Good Morning";
+  } else if (HH > 12 && HH < 17) {
+    return "Good Afternoon"; 
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage (sometext) {
+  var element = document.getElementById("greeting");
+  element.innerText = `${sometext}`;
+}
